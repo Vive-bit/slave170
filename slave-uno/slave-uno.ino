637c6eb51c8872acc8ee6c16824d420f54e047dd
Martin L. <77213543+Vive-bit@users.noreply.github.com>
2025-08-02 23:28:20 +0200

    fix

--- slave-uno/slave-uno.ino
+++ slave-uno/slave-uno.ino
@@ -287,7 +287,7 @@ bool handleRequest(const uint8_t* f) {
         analogWrite(pin, val);
       } else {
         setCurrentBlinkState(ERROR);
-        return;
+        return false;
       }
 
       uint8_t payload[] = { byte(val >> 8), byte(val) };
